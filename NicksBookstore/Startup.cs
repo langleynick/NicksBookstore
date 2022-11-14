c6b503ec5741883b25dfd332d083fa3466258164
langleynick <w0106967@myscc.ca>
2022-11-13 21:31:07 -0500

    Added UnitOfWork class and interface, and changed startup.cs to include UnitOfWork Made ISP_Call.cs interface public *BUG FIX*

--- NicksBookstore/Startup.cs
+++ NicksBookstore/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NicksBooks.DataAccess.Repository;
+using NicksBooks.DataAccess.Repository.IRepository;
 using NicksBookstore.DataAccess.Data;
 using System;
 using System.Collections.Generic;
@@ -34,6 +36,7 @@ namespace NicksBookstore
 
             services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddControllersWithViews();
         }
 
