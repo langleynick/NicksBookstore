617f0dac8f4d6922576c114604088ded2e9571d3
langleynick <w0106967@myscc.ca>
2022-11-24 15:55:20 -0500

    Added CoverType repository interface and class

---
+++ NicksBooks.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,30 @@
+﻿using NicksBooks.DataAccess.Repository.IRepository;
+using NicksBooks.Models;
+using NicksBookstore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NicksBooks.DataAccess.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CoverTypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(CoverType coverType)
+        {
+            //use .NET LINQ to retrieve the first or default category object
+            //then pass the id as a generic entity which matches the cover type ID
+            var objFromDb = _db.CoverType.FirstOrDefault(s => s.Id == coverType.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = coverType.Name;
+            }
+        }
+    }
+}
