bcb00999c73f5b21a791b7b5b8b0753fded56843
langleynick <W0106967@myscc.ca>
2022-12-05 13:04:42 -0500

    Edited SD.cs call to Get_CoverTypes, TODO: fix error on covertype upsert. Possibly needed to change application DbSet

--- NicksBooks.Utility/SD.cs
+++ NicksBooks.Utility/SD.cs
@@ -10,7 +10,7 @@ namespace NicksBooks.Utility
     {
         public const string Proc_CoverType_Create = "usp_CreateCoverType";
         public const string Proc_CoverType_Get = "usp_GetCoverType";
-        public const string Proc_CoverType_GetAll = "usp_GetCoverType";
+        public const string Proc_CoverType_GetAll = "usp_GetCoverTypes";
         public const string Proc_CoverType_Update = "usp_UpdateCoverType";
         public const string Proc_CoverType_Delete = "usp_DeleteCoverType";
     }
